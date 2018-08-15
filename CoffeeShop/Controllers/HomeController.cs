459d83194184054a92d22d8a6fdb7bc46127b0a6
Zachary_Theodore <fk9836@wayne.edu>
2018-08-15 18:34:22 -0400

    try 2

---
+++ CoffeeShop/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CoffeeShop.Models;
+
+namespace CoffeeShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        public ActionResult AddUser(UserInfo newUser)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.ConfMessage = "Thanks for the Information " + newUser.FirstName;
+
+                return View("Confirm");
+            }
+            else
+            {
+
+            return View("Error");
+            }
+        }
+    }
+}
---
+++ CoffeeShop/Models/UserInfo.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShop.Models
+{
+    public class UserInfo
+    {
+        [Required]
+        [RegularExpression("^[A-Za-z]{1,30}$")]
+        public string FirstName { set; get; }
+
+        [Required]
+        [RegularExpression("^[A-Za-z]{1,30}$")]
+        public string LastName { set; get; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9]{1,30}@[a-zA-Z0-9]{1,10}.[a-z]{2,3}$")]
+        public string Email { set; get; }
+
+        [Required]
+        [RegularExpression(@"^\d{3}-\d{3}-\d{4}|\d{10}|\d{3} \d{3} \d{4}$")]
+        public string PhoneNumber { set; get; }
+
+        [Required]
+        [RegularExpression(@"^[a-zA-Z0-9]{8,16}$")]
+        public string Password { set; get; }
+
+        public UserInfo()
+        {
+            FirstName = "";
+            LastName = "";
+            Email = "";
+            PhoneNumber = "";
+            Password = "";
+        }
+
+        public UserInfo(string fn, string ln, string em, string pn, string pw)
+        {
+            FirstName = fn;
+            LastName = ln;
+            Email = em;
+            PhoneNumber = pn;
+            Password = pw;
+        }
+
+
+
+
+    }
+}
